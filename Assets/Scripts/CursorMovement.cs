4a848127bb8c410c1e95e96312e43ad516cbb70d
Jeremy Pople <jpople@gmail.com>
2022-08-26 00:13:29 -0400

    add edge-looping and fix floating point drift

--- Assets/Scripts/CursorMovement.cs
+++ Assets/Scripts/CursorMovement.cs
@@ -8,7 +8,6 @@ public class CursorMovement : MonoBehaviour
 
     public float moveTime = 0.1f;
     bool isMoving;
-    Vector3 startPosition, endPosition;
 
     GameManager manager;
     PuzzleGenerator puzzle;
@@ -22,33 +21,68 @@ public class CursorMovement : MonoBehaviour
     }
 
     private void Update() {
-        // TODO: instead of multiplying by tileScaling, find the precise center of the tile in that position and set movement destination to that
-        // also TODO: implement logic to keep cursor in board
-        // also TODO: update isSelected of tiles appropriately
+        // also also TODO: update isSelected of tiles appropriately
+        // TODO: this shit can *super* be refactored into a nicer external Move() method that can also handle the tiles' isSelected
+        
+        Vector3 destinationPosition;
+        Vector2Int destination;
+
         if (Input.GetKey("w") && !isMoving) {
-            StartCoroutine(MoveCursor(Vector3.up * puzzle.tileScaling));
+            destination = MovementTarget(position, Vector2Int.up);
+            destinationPosition = GetTransformPosition(destination);
+            StartCoroutine(MoveCursor(destinationPosition));
+            position = destination;
         }
         if (Input.GetKey("a") && !isMoving) {
-            StartCoroutine(MoveCursor(Vector3.left * puzzle.tileScaling));
+            destination = MovementTarget(position, Vector2Int.left);
+            destinationPosition = GetTransformPosition(destination);
+            StartCoroutine(MoveCursor(destinationPosition));
+            position = destination;
         }
         if (Input.GetKey("s") && !isMoving) {
-            StartCoroutine(MoveCursor(Vector3.down * puzzle.tileScaling));
+            destination = MovementTarget(position, Vector2Int.down);
+            destinationPosition = GetTransformPosition(destination);
+            StartCoroutine(MoveCursor(destinationPosition));
+            position = destination;
         }
         if (Input.GetKey("d") && !isMoving) {
-            StartCoroutine(MoveCursor(Vector3.right * puzzle.tileScaling));
+            destination = MovementTarget(position, Vector2Int.right);
+            destinationPosition = GetTransformPosition(destination);
+            StartCoroutine(MoveCursor(destinationPosition));
+            position = destination;
         }
     }
 
-    private IEnumerator MoveCursor(Vector3 direction) {
+    Vector2Int MovementTarget(Vector2Int start, Vector2Int direction) { // "clips" movement to make edges of board loop
+        Vector2Int target = start + direction;
+        if (direction == Vector2Int.up && start.y == puzzle.boardHeight - 1) {
+            target.y = 0;
+        }
+        else if (direction == Vector2Int.down && start.y == 0) {
+            target.y = puzzle.boardHeight - 1;
+        }
+        if (direction == Vector2Int.right && start.x == puzzle.boardWidth - 1) {
+            target.x = 0;
+        }
+        else if (direction == Vector2Int.left && start.x == 0) {
+            target.x = puzzle.boardWidth - 1;
+        }
+        return target;
+    }
+
+    Vector3 GetTransformPosition(Vector2Int coords) { // converts a Vector2Int tile position to a transform position
+        return BoardManager.board[coords.x, coords.y].baseObject.transform.position;
+    }
+
+    private IEnumerator MoveCursor(Vector3 destination) {
         isMoving = true;
 
         float elapsedTime = 0f;
 
-        startPosition = transform.position;
-        endPosition = startPosition + direction;
+        Vector3 startPosition = transform.position;
         
         while(elapsedTime < moveTime) {
-            transform.position = Vector3.Lerp(startPosition, endPosition, (elapsedTime / moveTime));
+            transform.position = Vector3.Lerp(startPosition, destination, (elapsedTime / moveTime));
             elapsedTime += Time.deltaTime;
             yield return null;
         }

f2b1852cd540643cfdbf4fcb6e754438b93bcb44
Jeremy Pople <jpople@gmail.com>
2022-09-08 13:40:05 -0400

    implements object-based actions

--- Assets/Scripts/CursorMovement.cs
+++ Assets/Scripts/CursorMovement.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class CursorMovement : MonoBehaviour
 {
-    GameManager manager;
+    public GameManager manager;
     PuzzleGenerator puzzle;
+
+    Queue<LevelAction> actionQueue;
     
     public Vector2Int position;
 
@@ -29,11 +31,11 @@ public class CursorMovement : MonoBehaviour
     private void Start() {
         manager = GameObject.Find("GameManager").GetComponent<GameManager>();
         puzzle = GameObject.Find("GameManager").GetComponent<PuzzleGenerator>();
+        actionQueue = new Queue<LevelAction>();
     }
 
     private void Update() {
-        // also also TODO: update isSelected of tiles appropriately
-        // TODO: this shit can *super* be refactored into a nicer external Move() method that can also handle the tiles' isSelected
+        // handle updating input prompts (does this need to be here?)
         if (!BoardManager.GetTile(position).isRevealed) {
             manager.interactText.text = "reveal";
         }
@@ -42,30 +44,36 @@ public class CursorMovement : MonoBehaviour
         }
         manager.cancelPrompt.SetActive(!isSelecting); 
 
-        if (Input.GetKeyDown("w") && !isMoving) {
-            Move(Vector2Int.up);
-        }
-        if (Input.GetKeyDown("a") && !isMoving) {
-            Move(Vector2Int.left);
-        }
-        if (Input.GetKeyDown("s") && !isMoving) {
-            Move(Vector2Int.down);
-        }
-        if (Input.GetKeyDown("d") && !isMoving) {
-            Move(Vector2Int.right);
-        }
-        if(Input.GetKeyDown("e") && !isMoving) {
-            HandleInteract();
-        }
-        if(Input.GetKeyDown("q") && !isMoving) {
-            CancelSelection();
-        }
-        if(Input.GetKeyDown("f") && !isMoving) {
-            Finish();
+        // take in inputs and queue appropriate actions
+        if(!isMoving) {
+            if (Input.GetKeyDown("w")) {
+                new MoveAction(this, Vector2Int.up).Execute();
+            }
+            if (Input.GetKeyDown("a")) {
+                new MoveAction(this, Vector2Int.left).Execute();
+            }
+            if (Input.GetKeyDown("s")) {
+                new MoveAction(this, Vector2Int.down).Execute();
+            }
+            if (Input.GetKeyDown("d")) {
+                new MoveAction(this, Vector2Int.right).Execute();
+            }
+            if(Input.GetKeyDown("e")) {
+                new InteractAction(this).Execute();
+            }
+            if(Input.GetKeyDown("q")) {
+                new CancelAction(this).Execute();
+            }
+            if(Input.GetKeyDown("f")) {
+                new FinishAction(this).Execute();
+            }
         }
     }
 
-    Vector2Int MovementTarget(Vector2Int start, Vector2Int direction) { // "clips" movement to make edges of board loop
+    // are MovementTarget and GetTransformPosition better placed somewhere else?  BoardManager maybe?
+    // neither of them cares about anything in our class
+
+    public Vector2Int MovementTarget(Vector2Int start, Vector2Int direction) { // "clips" movement to make edges of board loop
         Vector2Int target = start + direction;
         if (direction == Vector2Int.up && start.y == puzzle.boardHeight - 2) {
             target.y = 1;
@@ -82,7 +90,7 @@ public class CursorMovement : MonoBehaviour
         return target;
     }
 
-    Vector3 GetTransformPosition(Vector2Int coords) { // converts a Vector2Int tile position to a transform position
+    public Vector3 GetTransformPosition(Vector2Int coords) { // converts a Vector2Int tile position to a transform position
         return BoardManager.board[coords.x, coords.y].baseObject.transform.position;
     }
 
@@ -106,7 +114,7 @@ public class CursorMovement : MonoBehaviour
         isSelecting = true;
     }
 
-    void Move(Vector2Int direction) {
+    public void Move(Vector2Int direction) {
         Vector2Int destination = MovementTarget(position, direction);
         Vector3 destinationPosition = GetTransformPosition(destination);
         StartCoroutine(MoveCursor(destinationPosition));
@@ -116,7 +124,7 @@ public class CursorMovement : MonoBehaviour
         }
     }
 
-    void HandleInteract() {
+    public void HandleInteract() {
         if(!BoardManager.GetTile(position).isRevealed && !isRevealing) {
             BoardManager.GetTile(position).isRevealed = true;
             StartCoroutine(DestroyCover());
@@ -141,14 +149,14 @@ public class CursorMovement : MonoBehaviour
         }
     }
 
-    void CancelSelection() {
+    public void CancelSelection() {
         cancelAudio.Play();
         Destroy(GameObject.Find("Selected(Clone)"));
         BoardManager.selectedTile = null;
         isSelecting = true;
     }
 
-    void Finish() {
+    public void Finish() {
         finishAudio.Play();
         puzzle.flowRate = 20;
     }
---
+++ Assets/Scripts/LevelAction.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelAction
+{
+    public CursorMovement movement;
+    public virtual void Execute() {
+        // nothing happens
+    }
+}
+
+public class MoveAction : LevelAction {
+    Vector2Int direction;
+
+    public MoveAction(CursorMovement movement, Vector2Int direction) {
+        this.movement = movement;
+        this.direction = direction;
+    }
+
+    public override void Execute() {
+        movement.Move(direction);
+    }
+}
+
+public class InteractAction : LevelAction {
+    public InteractAction(CursorMovement movement) {
+        this.movement = movement;
+    }
+
+    public override void Execute() {
+        movement.HandleInteract();
+    }
+}
+
+public class CancelAction : LevelAction {
+    public CancelAction(CursorMovement movement) {
+        this.movement = movement;
+    }
+
+    public override void Execute() {
+        movement.CancelSelection();
+    }
+}
+
+public class FinishAction : LevelAction {
+    public FinishAction(CursorMovement movement) {
+        this.movement = movement;
+    }
+
+    public override void Execute() {
+        movement.Finish();
+    }
+}
